7b37683e7c172623848fc04de402e603f1f43967
Markussim <you@example.com>
2020-09-02 16:57:55 +0200

    Added index file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const upload = require('express-fileupload')
 const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + 'index.html')
+
 })
 app.listen(port, () => console.log(`Example app listening on port port!`))
