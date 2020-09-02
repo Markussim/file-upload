c2dbcde5e27764f59deaebd741bc1374f21ec207
Markussim <you@example.com>
2020-09-02 16:58:32 +0200

    Added slash

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html')
+    res.sendFile(__dirname + '/index.html')
 
 })
 app.listen(port, () => console.log(`Example app listening on port port!`))
