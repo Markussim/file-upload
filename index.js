bd073c91c03ab4c54e82ea5e1379ee7efaf17288
Markussim <you@example.com>
2020-09-02 17:02:20 +0200

    Added console log

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const upload = require('express-fileupload')
 const app = express()
 const port = 5000
 
+app.use(upload())
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 
 })
+
+app.post('/', (req, res) => {
+    if (req.files) {
+        console.log(req.files)
+    }
+})
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
