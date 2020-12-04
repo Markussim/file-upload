a235f4dcef36c7324927cf3ed2cf93a15b33b4ca
Markussim <33492046+Markussim@users.noreply.github.com>
2020-12-04 15:22:38 +0100

    Revert "Merge pull request #2 from Markussim/s3"
    This reverts commit 1ee0d03c8c8bdb3a72706d812798782b921eec2a, reversing
    changes made to 9f7c8ceca51f6897a59cbcf847b47e6bb942066b.

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,75 @@
-import express, { Application } from "express";
-import upload, { FileArray } from "express-fileupload";
-import bodyParser from "body-parser";
-import fs from "fs";
-import mime from "mime-types";
-var AWS = require("aws-sdk");
-const creds: any = require("../credentials.json");
-const app: Application = express();
-const port = 5000;
-const clientdir: string = __dirname.substr(0, __dirname.length - 4) + "/client";
-const uploaddir: string = __dirname.substr(0, __dirname.length - 4) + "/upload";
+import express, { Application } from 'express'
+import upload, { FileArray } from 'express-fileupload'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+import mime from 'mime-types'
+const app: Application = express()
+const port = 5000
+const clientdir: string = __dirname.substr(0, __dirname.length - 4) + "/client"
+const uploaddir: string = __dirname.substr(0, __dirname.length - 4) + "/upload"
 
-var s3 = new AWS.S3({
-  accessKeyId: creds.id,
-  secretAccessKey: creds.pass,
-});
+app.use(upload())
 
-app.use(upload());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.get('/', (_req: any, res: any) => {
+    res.sendFile(clientdir + '/index.html')
+})
 
-app.get("/", (_req: any, res: any) => {
-  res.sendFile(clientdir + "/index.html");
-});
+app.get('/image/*', (_req, _res) => {
+    _res.sendFile(getFile(_req.url.substring(7)))
+})
 
-app.get("/image/*", async (_req, _res) => {
-  let urlString: String = _req.url;
-  if (typeof urlString.substring(7) == "string") {
-    let data = await getFile(urlString.substring(7));
-    _res.type(".png").send(data.Body);
-  }
-});
+app.post('/', (_req, _res) => {
+    if (_req.files) {
+        let file: FileArray = _req.files
+        let filename = file.theFile.name
+        let filedata: Buffer = file.theFile.data
+        console.log(filename)
 
-app.post("/", (_req, _res) => {
-  if (_req.files) {
-    let file: FileArray = _req.files;
-    let filename = file.theFile.name;
-    let filedata: Buffer = file.theFile.data;
-    console.log(filename);
+        let fileExtention = mime.extension(file.theFile.mimetype)
 
-    let fileExtention = mime.extension(file.theFile.mimetype);
+        if(file.theFile.size < 100000000 && !(fileExtention == false)) {
+            let fileName = file.theFile.md5 + "." + fileExtention
+            let filepath = uploaddir + "/" + fileName
+            fs.writeFile(filepath, filedata, function (err) {
+                if (err) {
+                    return console.log(err)
+                }
+                console.log("Saved file")
 
-    if (file.theFile.size < 100000000 && !(fileExtention == false)) {
-      let fileName = file.theFile.md5 + "." + fileExtention;
-      let filepath = uploaddir + "/" + fileName;
+                _res.header('Content-Type','application/json');
+                /*_res.header("Access-Control-Allow-Headers", "*")
+                _res.header("Access-Control-Allow-Origin", "*")*/
 
-      let params = { Bucket: "markussim-production", Key: fileName, Body: filedata };
-
-      s3.putObject(params, function (err: any, data: any) {
-        if (err) {
-          console.log(err);
+                _res.send(`{ \"upload\": \"successful\", \"link\": \"/image/${fileName}\"}`)
+            })
         } else {
-          console.log("Successfully uploaded data to myBucket/myKey");
-          _res.header("Content-Type", "application/json");
-          _res.send(
-            `{ \"upload\": \"successful\", \"link\": \"/image/${fileName}\"}`
-          );
+            _res.send("{ \"upload\": \"failed\" }")
         }
-      });
-    } else {
-      _res.send('{ "upload": "failed" }');
+        
     }
-  }
-});
+})
 
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`Example app listening on port port!`))
 
 function generateP() {
-  var pass = "";
-  var str =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789";
+    var pass = '';
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 1; i <= 32; i++) {
-    var char = Math.floor(Math.random() * str.length + 1);
+    for (var i = 1; i <= 32; i++) {
+        var char = Math.floor(Math.random()
+            * str.length + 1);
 
-    pass += str.charAt(char);
-  }
+        pass += str.charAt(char)
+    }
 
-  return pass;
+    return pass;
 }
 
-async function getFile(fileName: any) {
-  var getParams: any = {
-    Bucket: "markussim-production", //replace example bucket with your s3 bucket name
-    Key: fileName, // replace file location with your s3 file location
-  };
-
-  console.log(fileName);
-
-  try {
-    return await s3.getObject(getParams).promise();
-  } catch (error) {
-    return false;
-  }
+function getFile(fileName: String) {
+    return uploaddir +  "/" + fileName
 }
